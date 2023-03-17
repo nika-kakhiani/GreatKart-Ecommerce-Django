f989bb829e5c68615e7a14056f58b1c40fe8c58b
User <nikakakhi@gmail.com>
2023-03-17 20:26:35 +0400

    django authentication

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -136,3 +136,9 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
+
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "djangotest1121@gmail.com"
+EMAIL_HOST_PASSWORD = "svvardrcirvgayvz"
+EMAIL_USE_TLS = True
